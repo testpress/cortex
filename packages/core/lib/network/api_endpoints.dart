d3e30a498f6d118b41daecba4ceede6e2c261834
syed-tp <syed@testpress.in>
2026-05-02 20:46:22 +0530

    fix(core): use v2.4 logout endpoint
    Update logout API path to /api/v2.4/auth/logout/.

--- packages/core/lib/network/api_endpoints.dart
+++ packages/core/lib/network/api_endpoints.dart
@@ -4,7 +4,7 @@ class ApiEndpoints {
   static const String login = '/api/v2.5/auth-token/';
   static const String generateOtp = '/api/v2.5/auth/generate-otp/';
   static const String verifyOtp = '/api/v2.5/auth/otp-login/';
-  static const String logout = '/api/v2.5/auth/logout/';
+  static const String logout = '/api/v2.4/auth/logout/';
   static const String resetPassword = '/api/v2.3/password/reset/';
   static const String userProfile = '/api/v2.5/me/';
   static const String courseList = '/api/v3/courses/';
